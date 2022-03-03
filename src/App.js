a43c86349d5e3a7d7646ce97b72a49f5ed417ad0
cassnova <daniel.rojascasanova@gmail.com>
2022-03-03 18:40:10 -0300

    Agregue funcion para borrar tweet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { fireStore } from './firebase/firebase';
@@ -7,29 +7,29 @@ function App() {
 
   const [data, setData] = useState([])
   const [tweet, setTweet] = useState({
-    tweet:"",
-    username:""
+    tweet: "",
+    username: ""
 
   })
 
-  useEffect(()=> {
+  useEffect(() => {
     fireStore.collection('tweets').get()
-    .then((snapshot)=>{
-      const docs = []
-      snapshot.forEach(doc => {
+      .then((snapshot) => {
+        const docs = []
+        snapshot.forEach(doc => {
 
-        const snap={
-          username:doc.data().username,
-          tweet:doc.data().tweet,
-          id:doc.id
-        }
-        
-        docs.push(snap)
+          const snap = {
+            username: doc.data().username,
+            tweet: doc.data().tweet,
+            id: doc.id
+          }
 
+          docs.push(snap)
+
+        })
+        setData(docs)
       })
-      setData(docs)
-    } )
-  },[]);
+  }, []);
 
   // const handleUsernameChange = (e) => {
   //   let newTweet = {
@@ -40,13 +40,13 @@ function App() {
   // };
 
   // const handleTweetChange = (e) => {
-    
+
   //   let newTweet = {
   //     tweet: e.target.value,
   //     ...tweet
   //   };
   //   setTweet(newTweet);
-    
+
   // }
 
   const handleChange = (e) => {
@@ -59,33 +59,42 @@ function App() {
 
 
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault()
     const addTweet = fireStore.collection("tweets").add(tweet)
-    const getDoc = addTweet.then(doc=> (doc.get()))
+    const getDoc = addTweet.then(doc => (doc.get()))
     getDoc.then(doc => {
       const currentTweet = {
-        username:doc.data().username,
-        tweet:doc.data().tweet,
-        id:doc.id
+        username: doc.data().username,
+        tweet: doc.data().tweet,
+        id: doc.id
       }
       setData([currentTweet, ...data])
 
     })
     setTweet({
-      tweet:"",
-      username:""
+      tweet: "",
+      username: ""
     })
-    
+
   }
 
   console.log(tweet)
-  
+
+  function deleteTweet(id) {
+    const updatedTweets = data.filter((tweet) => {
+      return tweet.id !== id
+    })
+    setData(updatedTweets)
+    fireStore.doc(`tweets/${id}`).delete();
+
+  }
+
   return (
     <div className="App">
 
       <form data={data} setData={setData} >
-        <textarea 
+        <textarea
           name='tweet'
           onChange={handleChange}
           value={tweet.tweet}
@@ -102,23 +111,24 @@ function App() {
             placeholder='Escribe tu username'
           />
           <button onClick={handleSubmit}>Enviar tweet</button>
-        </div>  
+        </div>
       </form>
 
 
       <h1>Twitterss</h1>
-        {data.map(item=> (
-          <div key={item.id}>
-            <p>
-              Username: <strong>{item.username}</strong>
-            </p>
-            
-            <p>{item.tweet}</p>
-            <hr/>
-            
-          </div>
-        ))}
-      
+      {data.map(item => (
+        <div key={item.id}>
+          <p>
+            Username: <strong>{item.username}</strong>
+          </p>
+
+          <p>{item.tweet}</p>
+          <button className='delete' onClick={() => deleteTweet(item.id)}>X</button>
+          <hr />
+
+        </div>
+      ))}
+
     </div>
   );
 }
