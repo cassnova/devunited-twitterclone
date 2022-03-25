05bf49c6c0cbea0e657173d5796397edec5076ac
cassnova <daniel.rojascasanova@gmail.com>
2022-03-24 22:37:32 -0300

    Probando tests

--- src/__test__/deleteTweet.test.js
+++ src/__test__/deleteTweet.test.js
@@ -2,6 +2,6 @@ import { deleteTweet } from "../App";
 describe("Funcion que elimina un tweet", () => {
     test("Hago click en el boton para eliminar el tweet y se deberia borrar", () => {
         const result = deleteTweet(true);
-        expect(result).toBqe(true);
+        expect(result).toBe(true);
     })
 })
